a40daac0f15818eca69112c81fbd150ab5294da9
milemotigo <mtigo@gmail.com>
2023-10-20 23:20:04 +0100

    returns a tuple

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 ''' Hypermedia pagination'''
+from typing import List
 
 Server = __import__('1-simple_pagination').Server
 
-def get_hyper(self)
+def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+    
