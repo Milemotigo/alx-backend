363a9776d0b971902d162757db7463aae880b70b
milemotigo <mtigo@gmail.com>
2023-10-21 00:23:35 +0100

    fix

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -37,12 +37,18 @@ class Server:
         else:
             return self.dataset()[start:end]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        dataset_page = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
         return {
-            "page_size": len(self.dataset()),
+            "page_size": len(dataset_page),
             "page": page,
-            "data": self.dataset()[page_size:page],
-            "next_page": self.dataset()[page],
-            "prev_page": self.dataset(),
-            "total_pages": self.dataset()
+            "data": dataset_page,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
         }
