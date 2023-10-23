dc59586c8bce0a19ff8f28ed6f7507cd0f702f37
milemotigo <mtigo@gmail.com>
2023-10-23 07:15:42 +0100

    hypermedia

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -5,8 +5,22 @@ import math
 from typing import List
 
 
-index_range = __import__('0-simple_helper_function').index_range
+#!/usr/bin/env python3
+'''a function named index_range that takes two integer
+    arguments page and page_size
+'''
+from typing import Tuple
 
+
+def index_range(page: int, page_size: int)  -> Tuple[int, int]:
+    '''
+        A function that that take two int and returns
+        a tuple
+    '''
+    start = (page - 1) * page_size
+    end = start + page_size
+
+    return (start, end)
 
 class Server:
     """Server class to paginate a database of popular baby names.
