fb9528c8df847cea4187ae560c1c010634b6ffa0
milemotigo <mtigo@gmail.com>
2023-10-23 21:04:51 +0100

    task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -38,11 +38,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
+        """
+        return the appropriate pages of the dataset
+        """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
 
         start, end = index_range(page, page_size)
-        if start >= len(self.dataset()) or end < 0:
-            return []
-        else:
-            return self.dataset()[start:end]
+        return self.dataset()[start:end]
