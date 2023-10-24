f971182cbca5e5d9501e691a722da2a54677ec32
milemotigo <mtigo@gmail.com>
2023-10-24 23:21:37 +0100

    0-basic_cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+a class BasicCache that inherits from BaseCaching and is a caching system
+'''
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
