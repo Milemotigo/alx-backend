a988e590e4d2f94af206b9ce719723eac2939bee
milemotigo <mtigo@gmail.com>
2023-10-25 16:57:07 +0100

    cacheme

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -3,20 +3,35 @@
 """
 
 from base_caching import BaseCaching
-from collections import OrderedDict
+
 
 class LRUCache(BaseCaching):
+    """
+     a class LRUCache that inherits from
+     BaseCaching and is a caching system
+    """
 
     def __init__(self):
         super().__init__()
-        self.cache_data = OrderedDict
+
     def put(self, key, item):
+        """
+        Must assign to the dictionary
+        self.cache_data the item value for the key key
+        """
         if key is None or item is None:
             return
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            print(f"DISCARD: {self.cache_data[key]}")
-            self.cache_data.popitem(last=False)
-    
+            lru_item = next(iter(self.cache_data))
+            print(f"DISCARD: {lru_item}")
+            del self.cache_data[lru_item]
+        self.cache_data[key] = item
+        lru_item = +1
+
     def get(self, key):
+        """
+        Must return the value in self.cache_data linked to key
+        """
         if key is None:
-            return self.cache_data(key, None)
+            return None
+        return self.cache_data.get(key, None)
