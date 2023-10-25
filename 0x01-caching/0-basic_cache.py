ecef417efd1e3589750782fc51b85fd1ec9c7f7c
milemotigo <mtigo@gmail.com>
2023-10-25 13:31:53 +0100

    cacheme

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,11 +10,18 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
 
     def put(self, key, item):
+        """
+        Must assign to the dictionary self.cache_data
+        the item value for the key key
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """Must return the value in self.cache_data
+        linked to key.
+        """
         if key is None:
             return None
         return self.cache_data.get(key, None)
