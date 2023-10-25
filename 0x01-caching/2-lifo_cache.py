2f9d024a6fd58a8e0eb21d6623e5c56576bb579f
milemotigo <mtigo@gmail.com>
2023-10-25 14:35:29 +0100

    cacheme

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -22,8 +22,6 @@ class LIFOCache(BaseCaching):
             return
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             last_key = list(self.cache_data.keys())[-1]
-            # for k, _ in self.cache_data.items():
-            #     last_key = k[-1]
             print(f"DISCARD: {last_key}")
             del self.cache_data[last_key]
         self.cache_data[key] = item
@@ -33,4 +31,5 @@ class LIFOCache(BaseCaching):
         Must return the value in self.cache_data linked to key
         """
         if key is None:
-            return self.cache_data.get(key, None)
+            return None
+        return self.cache_data.get(key, None)
