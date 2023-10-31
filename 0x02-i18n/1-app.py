7ac7078dcfa220eefe75fde9f94ecf37d0ef066b
milemotigo <mtigo@gmail.com>
2023-10-31 19:13:40 +0100

    task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""1. Basic Babel setup
+"""
+
+from flask_babel import Babel
+from flask import Flask
+
+
+app = Flask(__name__)
+
+babel = Babel(app)
+class Config:
+
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
