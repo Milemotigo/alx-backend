690bada34a122699a1c30fec0fad682bc07191d5
milemotigo <mtigo@gmail.com>
2023-10-31 19:29:48 +0100

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -12,7 +12,7 @@ babel = Babel(app)
 
 
 class Config:
-
+    """ a Config class that has a LANGUAGES"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
