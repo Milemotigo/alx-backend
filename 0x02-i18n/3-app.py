98fda7bb6cfb13bd5e328e900ef4fa87795b0800
milemotigo <mtigo@gmail.com>
2023-11-01 15:35:52 +0100

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,45 +1,36 @@
 #!/usr/bin/env python3
-'''Task 2: Get locale from request
-'''
-
+"""
+3. Parametrize templates
+"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-class Config:
-    '''Config class'''
-
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
 app = Flask(__name__)
-app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-def get_locale() -> str:
-    """Retrieves the locale for a web page.
+class Config:
+    """Configs for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-    Returns:
-        str: best match
-    """
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Determine the best match from supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index() -> str:
-    '''default route
-
-    Returns:
-        html: homepage
-    '''
-    return render_template("3-index.html")
+    """Home Page"""
+    return render_template('3-index.html')
 
 
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
